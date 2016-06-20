3098ce4bdc31a69b99b17eaff298c71a7515d36f
Kaspar Kadalipp <kaspar.kadalipp@gmail.com>
2016-06-20 17:33:20 +0300

    dice

--- src/dice/main.java
+++ src/dice/main.java
@@ -1,7 +1,7 @@
 package dice;
 
 public class main {
-	// ...
+	
 	public static void main(String[] args) {
 		//	ülesanne: on vaja veeretada täringut väga palju kordi (nt 100 000 korda)
 		//	selle käigus pane kirja mitu korda tuli tulemusi 
@@ -19,15 +19,43 @@ public class main {
 //		int sum = a + b;	
 //		System.out.println(sum);
 		
-		int n = 1000000;
+		
+		
+		int n = 100000;
 		int a=0;
 		int b=0;
 		int c=0;
 		int d=0;
 		int e=0;
 		int f=0;
+		int q=0;
+		int w=0;
+		int y=0;
+		int u=0;
+		int i=0;
+		int o=0;
 		
-		for(int counter = 1; counter<n;counter++){
+		for(int h = 0; h<n;h++){
+			int p = (int)(Math.random()*6)+1;
+
+			
+			if (p == 1){
+				++q;
+			}else if (p == 2){
+				++w;
+			}else if (p == 3){
+				++y;
+			}else if (p == 4){
+				++u;
+			}else if (p == 5){
+				++i;
+			}else if (p == 6){
+				++o;
+			}else{
+				System.out.println("wut");
+			}
+		}
+		for(int counter = 0; counter<n;counter++){
 			int g = (int)(Math.random()*6)+1;
 //			System.out.println(g);
 			
@@ -44,23 +72,49 @@ public class main {
 			}else if (g == 6){
 				++f;
 			}else{
-				System.out.println("wut");
+				System.out.println("");
 			}
-			
+		
 		}
-		System.out.println("1 " + a +" "+ (double)a/n*100 + "%"); // a/n*100 + "%"
+		System.out.println("1 " + a +" "+ (double)a/n*100 + "%"); 
 		System.out.println("2 " + b +" "+ (double)b/n*100 + "%");
 		System.out.println("3 " + c +" "+ (double)c/n*100 + "%");
 		System.out.println("4 " + d +" "+ (double)d/n*100 + "%");
 		System.out.println("5 " + e +" "+ (double)e/n*100 + "%");
 		System.out.println("6 " + f +" "+ (double)f/n*100 + "%");
+		System.out.println("");
+		System.out.println("1 " + q +" "+ (double)q/n*100 + "%"); 
+		System.out.println("2 " + w +" "+ (double)w/n*100 + "%");
+		System.out.println("3 " + y +" "+ (double)y/n*100 + "%");
+		System.out.println("4 " + u +" "+ (double)u/n*100 + "%");
+		System.out.println("5 " + i +" "+ (double)i/n*100 + "%");
+		System.out.println("6 " + o +" "+ (double)o/n*100 + "%");
 //				add(1, 3);
 
+		
+		
+//		int counter = 0;
+				
+		
+		
+//		int die1 =0;
+//		int die2 =0;
+		
+		
+//		while(!(die1==1) && !(die2==1)){
+//			 die1 =(int)(Math.random()*6)+1;
+//			 die2 =(int)(Math.random()*6)+1;
+//			counter++;
+//		}
+//		System.out.println(counter);
 	}
-	public static double pre(int a, int n){
-		return a/n*100;
-	}
+//	public static int die1 (){
+//		int h = (int)(Math.random()*6)+1;
+//		return h;
+//	}
+	
 //		public static void add(int a, int b){ 
 //			System.out.println(a+b);
 //		}
+
 }
