69a3049ad9ad8c7ed29aa5ad0cc463c9025cd4db
Kaspar Kadalipp <kaspar.kadalipp@gmail.com>
2016-06-21 18:26:39 +0300

    few improvements *karl

--- src/dice/Main.java
+++ src/dice/Main.java
@@ -2,17 +2,26 @@ package dice;
 
 import java.util.Arrays;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
+		// Tõenäosused:
+		// 1: 62.8%
+		// 2: 54.4%
+		// 3: 30.2%
+		// 4: 9.7%
+		// 5: 1.3%
 		int sides = 6; // täringul on 6 tahku
-		int dices = 5; // mul on 5 täringut
-		int rolls = 5000000; // viskan täringuid miljon korda kokku (per 1 täring)
-		int series = rolls / dices; // viskan täringut nii mitu seeriat (nt 5 täringut korraga)
+		int dicesOriginal = 5; // nii mitme täringu kaupa viskan (nt 5 korraga)
+		int dicesTemp;
+		int triesOriginal = 3; // mitu korda võimalus täringuid visata
+		int triesTemp;
+		int rolls = 100000; // viskan täringuid miljon korda kokku (per 1 täring)
+		int series = rolls / (dicesOriginal * triesOriginal); // viskan täringut nii mitu seeriat (nt 5 täringut korraga)
+		
 		int rollResult = 0; // ühe veeretuse tulemus
 		int numbers[] = new int[5]; // massiiv täringute tulemuse jaoks
 		int howManyWantedNumbers = 0; // mitu täringut olen õigesti juba ära visanud
-		int tries; // mitu korda võimalus täringuid visata	
 		int totalTimesSuccessful = 0; // mitu korda olin edukas oma üldeesmärgi saavutamisel
 		
 		int howManyOfAll = 1; // mitu õiget peab olema kõigist (kas jahin 5t õiget 5st vmt).
@@ -20,23 +29,23 @@ public class main {
 		// 5t täringut viskan 200 tuhat korda
 		for(int i = 0; i < series; i++){
 			// uus visketsükkel ja numbritel reset, kuna uus katse (5 täringut, 3 katset)
-			tries = 3; 
-			dices = 5;
+			triesTemp = triesOriginal; 
+			dicesTemp = dicesOriginal;
 			// senikaua viskan kuni visete korrad on otsas või pole ühtegi täringut visata
-			while(tries > 0 && howManyWantedNumbers < howManyOfAll){ // NB! &&, muidu timestothrow läheb negatiivseks kuna pole veel saanud vajaminevaid tulemusi
+			while(triesTemp > 0 && howManyWantedNumbers < howManyOfAll){ // NB! &&, muidu timestothrow läheb negatiivseks kuna pole veel saanud vajaminevaid tulemusi
 				// viskan nii palju täringuid ükshaaval kui on täringuid ette nähtud (nt 5 asemel 3)
-				numbers = new int[dices];
-				for(int j=0;j<dices;j++){
+				numbers = new int[dicesTemp];
+				for(int j=0;j<dicesTemp;j++){
 					rollResult = (int)(Math.random()*sides)+1;
 					numbers[j]=rollResult;
 				} // for
 				for(int wantThisNumber : numbers){
 					if(wantThisNumber==4){
 						howManyWantedNumbers++;
-						dices--;	
+						dicesTemp--;	
 					} // if
 				} // for
-				tries--;
+				triesTemp--;
 				//System.out.println("timesToThrow " + tries + " result " + result);
 				if(howManyWantedNumbers == howManyOfAll){
 					totalTimesSuccessful++;
@@ -48,7 +57,8 @@ public class main {
 			howManyWantedNumbers=0;
 		} // for
 		
-		System.out.println("Times reached all 5 of same with 3 throws: " + totalTimesSuccessful);
+		System.out.println("Kokku täringuid visata: " + rolls + " ja see tähendab, et on seeriaid: " + series);
+		System.out.println("Tahan " + dicesOriginal + "-st saada " + triesOriginal + "-e viskega " + howManyOfAll + ", tulemus: " + totalTimesSuccessful);
 		System.out.println("Percentage: " +  ((double) totalTimesSuccessful / (double) series * 100) + "%");
 
 	}
