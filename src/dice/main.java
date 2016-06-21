62ebcb14d87a2dd00351fd0d4309d3e16c7d114c
Kaspar Kadalipp <kaspar.kadalipp@gmail.com>
2016-06-21 17:23:34 +0300

    kaspari variant

--- src/dice/main.java
+++ src/dice/main.java
@@ -30,48 +30,74 @@ public class main {
 		int numberOfSides = 6;
 		int numberOfTries = 1; // mitu korda võimalus täringuid visata	
 		int numberOfDice=5;
-		int numberOfRolls = 1000000;
+		int numberOfRolls = 100;
 		int want1 = 4;
 		int want2 = 4;
 		int want3 = 4;
 		int want4 = 4;
 		int want5 = 4;
 		int numbers[] = new int[5];
-		int sum;
-		
+		int result =0;
+		int timesToThrow;
+		int counter = 0; // mitmes vise , mitu "4", mitu täringut veeretasin , tulemus
+
+		// 5t täringut viskan 200 tuhat korda
 		for(int i = 0; i<numberOfRolls/5; i++){
-			die1 = (int)(Math.random()*numberOfSides)+1;
-			die2 = (int)(Math.random()*numberOfSides)+1;
-			die3 = (int)(Math.random()*numberOfSides)+1;
-			die4 = (int)(Math.random()*numberOfSides)+1;
-			die5 = (int)(Math.random()*numberOfSides)+1;
-			numbers[0]=die1;
-			numbers[1]=die2;
-			numbers[2]=die3;
-			numbers[3]=die4;
-			numbers[4]=die5;
-			sum =0;
-			for(int k : numbers){
-				if(k==4){
-					sum++;
-				}
-				
-			}
-			
-		}
-		
-//		int results[] =  new int[numberOfSides];
-//		results[4] = 10;
-//		results[0]= 3;
+			// uus visketsükkel
+			timesToThrow=3;
+			numberOfDice=5;
+			// senikaua viskan kuni visete korrad on otsas või pole ühtegi täringut visata
+			while(timesToThrow > 0 || result < 5){
+				// viskan nii palju täringuid ükshaaval kui on täringuid ette nähtud (nt 5 asemel 3)
+				numbers = new int[numberOfDice];
+				for(int j=0;j<numberOfDice;j++){
+					die1 = (int)(Math.random()*numberOfSides)+1;
+					numbers[j]=die1;
+				} // for
+				for(int k : numbers){
+					if(k==4){
+						result++;
+						numberOfDice--;	
+					} // if
+				} // for
+				timesToThrow--;
+			} // while
+			if(result==5){
+				counter++;
+			} // if
+			System.out.print("mitu korda saan veel visata: " + timesToThrow );
+			System.out.println(" tulemused: " + Arrays.toString(numbers) );
+			result=0;
+		} // for
+		//				System.out.println(Arrays.toString(results));
+		//			die1 = (int)(Math.random()*numberOfSides)+1;
+		//			die2 = (int)(Math.random()*numberOfSides)+1;
+		//			die3 = (int)(Math.random()*numberOfSides)+1;
+		//			die4 = (int)(Math.random()*numberOfSides)+1;
+		//			die5 = (int)(Math.random()*numberOfSides)+1;
+		//			numbers[0]=die1;
+		//			numbers[1]=die2;
+		//			numbers[2]=die3;
+		//			numbers[3]=die4;
+		//			numbers[4]=die5;
+
+
+
+
+
+
+		//		int results[] =  new int[numberOfSides];
+		//		results[4] = 10;
+		//		results[0]= 3;
 		// vaja näidata kõik võimalused
 
 
-//		for(int counter =0; counter<numberOfRolls;counter++){
-//			die1 = (int)(Math.random()*numberOfSides)+1;
-//			results[counter] = die1;	
-//			results[die1-1]++;
-//		}
-//		System.out.println(Arrays.toString(results));
+		//		for(int counter =0; counter<numberOfRolls;counter++){
+		//			die1 = (int)(Math.random()*numberOfSides)+1;
+		//			results[counter] = die1;	
+		//			results[die1-1]++;
+		//		}
+		//		System.out.println(Arrays.toString(results));
 
 		/*
 		for(int counter =0; counter<1000000;counter++){
@@ -99,10 +125,10 @@ public class main {
 //	}
 		 */
 		// for loop
-//		luckyRoll*=numberOfTries;
-//		System.out.println(numberOfDice + " of the same number");
-//		System.out.print("rounds played " + rounds );
-//		System.out.println(" rounds won " + luckyRoll);
-//		System.out.println("probability: " +(double)luckyRoll/rounds*100 + "%" );
+		//		luckyRoll*=numberOfTries;
+		//		System.out.println(numberOfDice + " of the same number");
+		//		System.out.print("rounds played " + rounds );
+		//		System.out.println(" rounds won " + luckyRoll);
+		//		System.out.println("probability: " +(double)luckyRoll/rounds*100 + "%" );
 	}
 }
